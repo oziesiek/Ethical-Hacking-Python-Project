bef8f85f885ba6a599359f9fba45b2820debf16a
Paweł Orzechowski <110523018+oziesiek@users.noreply.github.com>
2023-09-13 11:20:11 +0200

    Update mac_changer.py
    comments

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -4,7 +4,7 @@ import subprocess
 import optparse
 import re
 
-
+# Function to parse command-line arguments
 def get_arguments():
     parser = optparse.OptionParser()
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
@@ -16,22 +16,22 @@ def get_arguments():
         parser.error("[-] Please specify a new MAC, use --help for more info.")
     return options
 
-
+# Function to change the MAC address of a given interface
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "down"])  # Disable the interface
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])  # Set the new MAC address
+    subprocess.call(["ifconfig", interface, "up"])  # Enable the interface
 
+# Function to get the current MAC address of a given interface
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode()
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode()  # Get ifconfig output
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)  # Search for MAC address
 
     if mac_address_search_result:
-        return mac_address_search_result.group(0)
+        return mac_address_search_result.group(0)  # Return the found MAC address
     else:
-        print("[-] Could not read MAC address.")
-
+        print("[-] Could not read MAC address.")  # Print error message if MAC address not found
 
 options = get_arguments()
 
@@ -42,6 +42,6 @@ change_mac(options.interface, options.new_mac)
 
 current_mac = get_current_mac(options.interface)
 if current_mac == options.new_mac:
-    print("[+] MAC address was succesfully changed to " + current_mac)
+    print("[+] MAC address was successfully changed to " + current_mac)
 else:
-    print("[-] MAC addres did not get changed")
+    print("[-] MAC address did not get changed")  # Fix typo: "addres" to "address"
